48e812fdbe020c2adbbb1e3a03b5c1fd2f6b85e2
Jon Stites <jonstites2@gmail.com>
2015-07-24 15:53:52 -0700

    clone oh-my-zsh recursively

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
+1;2802;0c#!/usr/bin/env bash
 
 
 command -v zsh >/dev/null 2>&1 || { echo >&2 "I require zsh but it's not installed.  Aborting."; exit 1; }
 
-git clone https://github.com/zsh-users/antigen.git zsh-antigen
+git clone --recursive https://github.com/zsh-users/antigen.git zsh-antigen
 
 ./makesymlinks.sh
