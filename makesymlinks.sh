6cf9e4c9eabf1991bd991dac82f076c72b36459a
Jonathan Stites <mail@jonstites.com>
2017-06-18 17:33:29 -0500

    add a gitconfig

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=$(dirname $0)                    # dotfiles directory
 olddir=~/.dotfiles_old             # old dotfiles backup directory
-files="bashrc bash_profile emacs zshrc zsh-antigen tmux.conf jonstites.zsh_theme"    # list of files/folders to symlink in homedir
+files="bashrc bash_profile emacs zshrc zsh-antigen tmux.conf jonstites.zsh_theme gitconfig"    # list of files/folders to symlink in homedir
 
 ##########
 
